d5d006afe0806a70b5bdb9ebd2a8669aec4ecaad
chidambararaja.b <chidambararaja.b@accubits.com>
2023-04-16 00:37:35 +0530

    [API]

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { Effect, Reducer, Ref } from "@/hooks";
 import Callback from "@/hooks/Callback";
 import { Memo } from "@/hooks/Memo";
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div>
       {/* <Ref /> */}
@@ -10,6 +10,26 @@ export default function Home() {
       {/* <Reducer /> */}
       {/* <Callback /> */}
       {/* <Memo /> */}
+      <div>
+        <h1>Latest Posts:</h1>
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const { data } = await axios.get(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  console.log({ data });
+  return {
+    props: {
+      posts: data.slice(0, 5), // get first 5 posts
+    },
+  };
 }
